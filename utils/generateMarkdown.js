1bb38f7f816a8924e0b759e6c75b7592f7b1b183
alamm01 <102335347+alamm01@users.noreply.github.com>
2023-12-26 12:15:45 -0600

    logo code related to license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,54 +1,95 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# Title 
-  ${data.title}
-
-
-  ## Table of Contents
-  1. [Description](#description)
-  2. [Installation](#installation)
-  3. [Usage](#usage)
-  4. [Contributing](#contributing)
-  5. [License](#license)
-  6. [Questions](#question)
-
-  ## Description
-  ${data.description}
-
-  ## Installation
-  ${data.installation}
-
-  ## Usage
-  ${data.usage}
-
-  ## License
-  ${data.license}
-
-  ## Contributing
-  ${data.contributing}
-
-  ## Testing
-  ${data.testing}
-
-  ## Question
-  Here's the information to contact me if you have further questions:
-
-  GitHub username: ${data.questionusername}  
-  Email: ${data.questionemail}
-
-`;
-}
-
-module.exports = generateMarkdown;
+// Function to return a license badge based on which license is passed in
+function renderLicenseBadge(license) {
+    let badge = '';
+    switch(license) {
+      case 'Apache License 2.0':
+        badge = '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+        break;
+      case 'GNU General Public License v3.0':
+        badge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+        break;
+      case 'MIT License':
+        badge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+        break;
+      case 'None':
+        badge = '';
+        break;
+    }
+    // console.log(`Badge for ${license}: ${badge}`);
+    return badge;
+  }
+  
+  // Function to return the license link
+  function renderLicenseLink(license) {
+    let link = '';
+    switch(license) {
+      case 'Apache License 2.0':
+        link = 'https://opensource.org/licenses/Apache-2.0';
+        break;
+      case 'GNU General Public License v3.0':
+        link = 'https://www.gnu.org/licenses/gpl-3.0';
+        break;
+      case 'MIT License':
+        link = 'https://opensource.org/licenses/MIT';
+        break;
+      case 'None':
+        link = '';
+        break;
+    }
+    return link;
+  }
+  
+  // Function to return the license section of README
+  function renderLicenseSection(license) {
+    if(license === 'None') {
+      return '';
+    } else {
+      return `This project is licensed under the ${license} - see the [LICENSE](${renderLicenseLink(license)}) file for details.`;
+    }
+  }
+  
+  // Function to generate markdown for README
+  function generateMarkdown(data) {
+    // Add license badge near the top of the README
+    const licenseBadge = renderLicenseBadge(data.license);
+    console.log(licenseBadge);
+    console.log("I am here");
+    return `# ${data.title}
+    ${licenseBadge}
+  
+    ## Table of Contents
+    1. [Description](#description)
+    2. [Installation](#installation)
+    3. [Usage](#usage)
+    4. [Contributing](#contributing)
+    5. [License](#license)
+    6. [Questions](#questions)
+  
+    ## Description
+    ${data.description}
+  
+    ## Installation
+    ${data.installation}
+  
+    ## Usage
+    ${data.usage}
+  
+    ## Contributing
+    ${data.contributing}
+  
+    ## License
+    ${renderLicenseSection(data.license)}
+  
+    ## Testing
+    ${data.testing}
+  
+    ## Questions
+    For any questions, please reach out to me at:
+  
+    GitHub: [${data.questionusername}](https://github.com/${data.questionusername})
+    Email: ${data.questionemail}
+    `;
+  }
+  
+  module.exports = generateMarkdown;
+  
